7c889cffb7b4da58460830ab11b01bef5dd15632
Brahim Ghaouthi <abraham.ght@hotmail.com>
2022-05-26 16:13:10 +0100

    create a stack based postfix expression evaluator

--- postfix.py
+++ postfix.py
@@ -1,30 +1,62 @@
+
+class Empty(Exception):
+    pass
+
+
+class ArrayStack:
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def push(self, e):
+        self._data.append(e)
+
+    def top(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()
+
+
 def post_fix(s):
+    stack = ArrayStack()
     l = []
-    l2 = []
-    operator = None
+
     for i in range(len(s)):
         l.append(s[i])
 
+
+
+
     for i in range(len(l)):
-        if l[i] in ['+', '-', '*', '/'] and l[i+1] in ['+', '-', '*', '/']:
-            l.insert(i-2, l[i+1])
-            l2.append(l[i+2])
-            l.pop(i+2)
-
-        elif l[i] in ['+', '-', '*', '/']:
-            operator = l[i]
-            l[i] = l[i-1]
-            l[i-1] = operator
-            exp = ''.join([l[i-2], l[i-1], l[i]])
+        if l[i] in ['+', '-', '*', '/']:
+            l2 = []
+            a = stack.pop()
+            b = stack.pop()
+            l2.append(b)
+            l2.append(l[i])
+            l2.append(a)
+            exp = ''.join(l2)
             res = eval(exp)
-            l.pop(i)
-            l.pop(i-1)
-            l.pop(i-2)
-            l.insert(i-2, str(res))
-            i-=2
+            stack.push(str(res))
 
 
-post_fix('5+2')
+        else:
+            stack.push((l[i]))
+
+    return float(stack.top())
+
+
+print(post_fix('52+83-*4/'))
 
 
 
